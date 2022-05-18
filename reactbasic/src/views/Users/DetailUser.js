88dbe9f9cd172ee8592ea6d8c366b58d8405ae74
tantripc <letantri2507@gmail.com>
2022-05-18 16:03:09 +0700

    Params

---
+++ reactbasic/src/views/Users/DetailUser.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class DetailUser extends React.Component {
+    render() {
+        return (
+            <div>DetailUser {!this.props && this.props}</div>
+        )
+    }
+}
+
+export default DetailUser;
