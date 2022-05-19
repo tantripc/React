6690a3d84852e405ff52e1f62cbf06e79e67d835
tantripc <letantri2507@gmail.com>
2022-05-19 15:58:27 +0700

    add redux

---
+++ reactbasic/src/stores/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+    user: [
+        { id: "1", name: "cc" },
+        { id: "2", name: "bb" },
+    ]
+};
+const rootReducer = (state = initState, action) => {
+    return state;
+}
+
+export default rootReducer;
--- reactbasic/src/views/App.js
+++ reactbasic/src/views/App.js
@@ -36,7 +36,6 @@ function App() {
               <Route path=':userId' element={<DetailUserParam />} />
             </Route>
           </Routes>
-          <Outlet />
         </header>
       </div>
     </BrowserRouter>
